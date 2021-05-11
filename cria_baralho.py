d2731aa8adfc94cd9f255284f3d890ad6b01d7ea
eb100 <eb100221@gmail.com>
2021-05-11 20:55:11 -0300

    ...

--- cria_baralho.py
+++ cria_baralho.py
@@ -8,21 +8,14 @@ import random
 
 def cria_baralho():
     lista = []
-    numeros = ['1','2','3','4','5','6','7','8','9','10']
-    naipe =['E','C','O','P']
+    numeros = ['1','2','3','4','5','6','7','8','9','10'] #lista de números
+    naipe =['E','C','O','P']#lista de naipes
     numeros_embaralhados = random.choice(numeros)
     naipes_embaralhadas=random.choice(naipe)
-    carta_embaralhas = numeros_embaralhados+naipes_embaralhadas
-    lista.append(carta_embaralhas)
+    carta_embaralhadas = numeros_embaralhados+naipes_embaralhadas #concatenamento de strings
+    lista.append(carta_embaralhadas)
     return lista
 
-x = dict(cria_baralho())
-
-print(x)
-print(type(x))
-for chave, valor in x.items():
-    print(chave)
-
-
+print(cria_baralho())
 
 
